9f5fa1c3b9f132967539411c89b9fb2e128cb36b
luankaike-code <luankaikemacksantos@gmail.com>
2026-04-13 14:58:58 -0300

    Verific if path isnt directory and emit this error

--- source/backend/utils/refGetter/getterfiles.cpp
+++ source/backend/utils/refGetter/getterfiles.cpp
@@ -8,6 +8,11 @@ GetterFiles::GetterFiles(QObject *parent) : QObject{parent} {}
 void GetterFiles::getAllImagesAtFolder(const QString& folder) {
     std::vector<QString> images;
 
+    if(!fs::is_directory(folder.toStdString())) {
+        emit invalidDirectoryPath(folder);
+        return;
+    }
+
     for (auto const& dir_entry : fs::directory_iterator(folder.toStdString())) {
         if (dir_entry.is_directory())
             continue;
--- source/backend/utils/refGetter/getterfiles.h
+++ source/backend/utils/refGetter/getterfiles.h
@@ -4,16 +4,20 @@
 #include <QObject>
 #include <QtQml/qqmlregistration.h>
 
+namespace fs = std::filesystem;
+
 class GetterFiles : public QObject
 {
     Q_OBJECT
     QML_ELEMENT
+
 public:
     explicit GetterFiles(QObject *parent = nullptr);
 
     Q_INVOKABLE void getAllImagesAtFolder(const QString& folder);
 signals:
     void gottedAllImagesAtFolder(std::vector<QString> images);
+    void invalidDirectoryPath(QString path);
 };
 
 #endif // GETTERFILES_H
