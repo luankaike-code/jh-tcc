8ad1ea7adbd35312e42d0c0b4e00a5c0019d2000
luankaike-code <luankaikemacksantos@gmail.com>
2026-05-18 21:00:45 -0300

    Update sessionModeReader verification logic

--- source/backend/helpers/sessionModeReader/sessionModeReader.cpp
+++ source/backend/helpers/sessionModeReader/sessionModeReader.cpp
@@ -3,11 +3,11 @@
 SessionModeReader::SessionModeReader(QObject* parent) : QObject(parent) {}
 
 bool SessionModeReader::hasTimerLimit(const SessionModes::Enum& sessionModeValue) {
-    return sessionModeValue != SessionModes::Enum::InfinityTime && sessionModeValue != SessionModes::Enum::Sandbox;
+    return sessionModeValue == SessionModes::Enum::InfinityImages || sessionModeValue == SessionModes::Enum::Normal;
 }
 
 bool SessionModeReader::hasImagesLimit(const SessionModes::Enum& sessionModeValue) {
-    return sessionModeValue != SessionModes::Enum::InfinityImages && sessionModeValue != SessionModes::Enum::Sandbox;
+    return sessionModeValue == SessionModes::Enum::InfinityTime || sessionModeValue == SessionModes::Enum::Normal;
 }
 
 bool SessionModeReader::hasRoadmap(const SessionModes::Enum& sessionModeValue) {
