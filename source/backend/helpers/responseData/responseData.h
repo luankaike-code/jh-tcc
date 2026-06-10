363a391293bea7054bbc35ff5ede6cb41ae9e3c7
Luan Kaike <luankaikemacksantos@gmail.com>
2026-06-10 09:27:31 -0300

    Create responseData class

---
+++ source/backend/helpers/responseData/responseData.h
@@ -0,0 +1,20 @@
+#ifndef RESPONSEDATA_H
+#define RESPONSEDATA_H
+
+template<typename T>
+class ResponseData {
+public:
+    enum Status {
+        OK,
+        ERROR_NOT_HANDLER,
+        ERROR_HANDLER
+    };
+
+    Status status;
+    T data;
+
+    ResponseData();
+    inline ResponseData(Status status, T data) : status(status), data(data) {};
+};
+
+#endif // RESPONSEDATA_H
